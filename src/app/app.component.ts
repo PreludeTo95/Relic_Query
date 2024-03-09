d041e397f3b2a96adc56f3b0064c5829c7dd7cf9
Aaron Tiger <tigeraaronm@gmail.com>
2024-03-08 21:20:49 -0500

    Added Angular table reading value from mock object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+export interface PrimeItems {
+  name: string;
+  price: string;
+  rarity: string;
+}
+
+const primeItems: PrimeItems[] = [
+  {name: 'Soma Prime Barrel', price: '25', rarity: 'common'},
+  {name: 'Soma Prime Stock', price: '20', rarity: 'common'},
+  {name: 'Soma Prime Reciever', price: '50', rarity: 'common'},
+  {name: 'Soma Prime Blueprint', price: '5', rarity: 'uncommon'},
+  {name: 'Trinity Prime Neuroptics', price: '100', rarity: 'uncommon'},
+  {name: 'Trinity Prime Systems', price: '15', rarity: 'rare'},
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +24,8 @@ import { FormControl } from '@angular/forms';
 
 export class AppComponent {
   title = 'Relic_Query';
+  displayedColumns: string[] = ['name', 'price'];
+  dataSource = primeItems;
 
   itemFormControl: FormControl = new FormControl(null)
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button'
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { MatButtonModule } from '@angular/material/button'
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
